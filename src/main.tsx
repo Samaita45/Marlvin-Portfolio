b76d90e30f89b67ffb38b2fdec73abc704776051
Marlvin Anesu Munyanyi <marlvinmunyanyi4@gmail.com>
2026-06-16 15:51:42 +0200

    Fix GitHub Pages routing with React Router basename

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,11 @@ import { ThemeProvider } from '@/hooks/useTheme'
 import App from '@/App'
 import './index.css'
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, '')
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename || undefined}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
